12b8f5fac65e2ba1bea8b1107e50665e17c95325
thangasblockchain <thangarajm@icloud.com>
2022-11-04 20:27:01 +0530

    adding git ignore

---
+++ rust_dev/target/debug/deps/rust_dev-0f17d82f3092e7c0.d
@@ -0,0 +1,5 @@
+/Users/thangarajmuruganantham/devspace/WBA-CosmWasm/IBCToken.W22MTW.Thangaraj/rust_dev/target/debug/deps/rust_dev-0f17d82f3092e7c0.rmeta: src/main.rs
+
+/Users/thangarajmuruganantham/devspace/WBA-CosmWasm/IBCToken.W22MTW.Thangaraj/rust_dev/target/debug/deps/rust_dev-0f17d82f3092e7c0.d: src/main.rs
+
+src/main.rs:
